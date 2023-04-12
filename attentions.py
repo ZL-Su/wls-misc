7b431553c029eca6bcf2843211ec6f05c969666c
su-zl <su-zl@seu.edu.cn>
2023-04-12 19:22:19 +0800

    Add module of SpatialAttention

--- attentions.py
+++ attentions.py
@@ -1,8 +1,10 @@
+import torch
 import torch.nn as nn
 
 class ChannelAttention(nn.Module):
     '''
-    @brief Impl of channel attention mechanism
+    Brief:
+      Impl of channel attention mechanism
     Args:
      - `in_planes` Channel planes of input feature map
      - `ratio` Reduction ratio of channels from input to output, i.e. `out_planes = ratio * in_planes`
@@ -26,5 +28,23 @@ class ChannelAttention(nn.Module):
         max_branch = self.mlp(self.max_pool(x))
         sum_out = avg_branch+max_branch
         return self.sigmoid(sum_out)
-        
+    
+class SpatialAttention(nn.Module):
+    '''
+    Brief:
+      Impl of spatial attention mechanism
+    Args:
+     - `kernel_size` Kernel size specified to perform convolution on catenated feature map 
+    '''
+    def __init__(self, kernel_size:int=7):
+        super().__init__(SpatialAttention, self)
+        assert kernel_size in (3,7), 'kernel size in `SpatialAttention` must be 3 or 7'
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
 
+    def forward(self, x):
+        avg = torch.mean(x, dim=1, keepdim=True)
+        max, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg, max], dim=1)
+        x = self.conv(x)
+        return self.sigmoid(x)
