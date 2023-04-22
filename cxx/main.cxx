cb69519ea304cc8a7341f4614398cc0440409ce6
su-zl <su-zl@seu.edu.cn>
2023-04-22 19:30:20 +0800

    Update main.cxx

--- cxx/main.cxx
+++ cxx/main.cxx
@@ -3,14 +3,12 @@
 #include <cstddef>
 #include "vector.h"
 
-static constexpr auto mem_size = 17045913600 / 1024 / 1024 / 1024;
-
 int main()
 {
     std::cout << "Hello World\n";
     std::cout << "Max alignment: " << alignof(std::max_align_t) << std::endl;
 
     auto vec = dyvp::Vector<float, 0>(10, 3.14159);
-    auto vec_sum = vec + vec;
+    auto vec_sum = dyvp::sum(vec + vec);
     return 0;
 }
